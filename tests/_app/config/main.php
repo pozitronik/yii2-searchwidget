fe9ff01b422541b3986930d61ceddd6a33b30f90
Pozitronik <antikillerxxl@gmail.com>
2022-11-07 15:47:03 +0400

    Added yii2 module

---
+++ tests/_app/config/main.php
@@ -0,0 +1,47 @@
+<?php /** @noinspection UsingInclusionReturnValueInspection */
+declare(strict_types = 1);
+
+use yii\caching\DummyCache;
+use yii\web\AssetManager;
+use yii\web\ErrorHandler;
+
+
+$config = [
+	'id' => 'basic',
+	'basePath' => dirname(__DIR__),
+	'bootstrap' => ['log'],
+	'aliases' => [
+		'@vendor' => './vendor',
+		'@bower' => '@vendor/bower-asset',
+		'@npm' => '@vendor/npm-asset',
+	],
+	'modules' => [
+	],
+	'components' => [
+		'request' => [
+			'cookieValidationKey' => 'sosijopu',
+		],
+		'cache' => [
+			'class' => DummyCache::class,
+		],
+		'errorHandler' => [
+			'class' => ErrorHandler::class,
+			'errorAction' => 'site/error',
+		],
+		'urlManager' => [
+			'enablePrettyUrl' => true,
+			'showScriptName' => false,
+			'rules' => [
+			],
+		],
+		'assetManager' => [
+			'class' => AssetManager::class,
+			'basePath' => '@app/assets'
+		],
+	],
+	'params' => [
+		'bsVersion' => '4'
+	],
+];
+
+return $config;
