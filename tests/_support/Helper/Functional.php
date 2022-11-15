8811377ee9f0f20f675c0327c0e125b803560b1d
Pozitronik <antikillerxxl@gmail.com>
2022-11-15 14:03:02 +0400

    Functional tester stuff

---
+++ tests/_support/Helper/Functional.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types = 1);
+
+namespace Helper;
+
+// here you can define custom actions
+// all public methods declared in helper class will be available in $I
+
+use Codeception\Module;
+
+/**
+ * Class Functional
+ * @package Helper
+ */
+class Functional extends Module {
+
+}
