c3a2d566175f5872caaed03506c67080617a55d9
Pozitronik <antikillerxxl@gmail.com>
2022-11-15 14:21:14 +0400

    JSON actions added

--- tests/_app/controllers/SiteController.php
+++ tests/_app/controllers/SiteController.php
@@ -3,14 +3,46 @@ declare(strict_types = 1);
 
 namespace app\controllers;
 
+use pozitronik\widgets\SearchAction;
 use Yii;
+use yii\filters\ContentNegotiator;
 use yii\helpers\Html;
 use yii\web\Controller;
+use yii\web\Response;
 
 /**
  * class SiteController
  */
 class SiteController extends Controller {
+
+	public $enableCsrfValidation = false;
+
+	/**
+	 * @inheritDoc
+	 */
+	public function behaviors():array {
+		return [
+			[
+				'class' => ContentNegotiator::class,
+				'formats' => [
+					'text/plain' => Response::FORMAT_JSON
+				],
+				'only' => [
+					'search', 'username-search'
+				]
+			],
+		];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function actions():array {
+		return [
+			'search' => SearchAction::class,
+			'username-search' => SearchAction::class
+		];
+	}
 
 	/**
 	 * @return string
